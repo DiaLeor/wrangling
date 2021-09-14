290718f4326c66abcebf6680033775ffbcfa2cc6
Dia Leor Paule <dia.paule@gmail.com>
2021-09-14 09:30:35 +0800

    Section 1 notes on data import

---
+++ data_import/data-import.R
@@ -0,0 +1,17 @@
+## Section 1 - Data Import
+
+# Paths and the Working Directory -----------------------------------------
+
+# .. Code ..
+
+
+# readr and readxl Packages -----------------------------------------------
+
+# .. Code ..
+
+# Importing Data Using R-Base Functions -----------------------------------
+
+
+# Downloading Files from the Internet -------------------------------------
+
+# .. Code ..
