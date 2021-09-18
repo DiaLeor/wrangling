75b36fbde726c875a517877eb483e4ca856b7bf2
Dia Leor Paule <dia.paule@gmail.com>
2021-09-18 19:28:14 +0800

    Update Section 3.1 notes on string processing

--- string_processing/string-p-1.R
+++ string_processing/string-p-1.R
@@ -15,6 +15,9 @@
 
 # .. Code ..
 # read in raw murders data from Wikipedia
+library(tidyverse)
+library(rvest)
+
 url <- "https://en.wikipedia.org/w/index.php?title=Gun_violence_in_the_United_States_by_state&direction=prev&oldid=810166167"
 murders_raw <- read_html(url) %>% 
   html_nodes("table") %>% 
@@ -27,7 +30,7 @@ head(murders_raw)
 class(murders_raw$population)
 class(murders_raw$total)
 
-# Defining Strings: Single,  Double,  Esc ---------------------------------
+# Defining Strings: Single/Double & Esc Quotes ---------------------------------
 
 # Define a string by surrounding text with either single quotes or double quotes.
 
@@ -42,11 +45,11 @@ class(murders_raw$total)
 # We will see additional uses of the escape later.
 
 # .. Code ..
-s <- "Hello!"    # double quotes define a string
-s <- 'Hello!'    # single quotes define a string
-s <- `Hello`    # backquotes do not
+s <- "Hello!" # double quotes define a string
+s <- 'Hello!' # single quotes define a string
+s <- `Hello`  # back quotes do not
 
-s <- "10""    # error - unclosed quotes
+#s <- "10""   # error - un-closed quotes
 s <- '10"'    # correct
 
 # cat shows what the string actually looks like inside R
@@ -56,11 +59,11 @@ s <- "5'"
 cat(s)
 
 # to include both single and double quotes in string, escape with \
-s <- '5'10"'    # error
-s <- "5'10""    # error
-s <- '5\'10"'    # correct
+#s <- '5'10"'  # error
+#s <- "5'10""  # error
+s <- '5\'10"'  # correct
 cat(s)
-s <- "5'10\""    # correct
+s <- "5'10\""  # correct
 cat(s)
 
 # stringr Package ---------------------------------------------------------
@@ -81,10 +84,6 @@ library(tidyverse)    # includes stringr
 
 # Case Study 1 ------------------------------------------------------------
 
-# ***NOTE: The Wikipedia page with US murder data has been updated since these notes were created and is no longer
-# easily converted with as.numeric() due to inclusion of endnote links. We will learn more string processing methods later
-# that can be used to remove these endnotes.***
-
 # Use the str_detect() function to determine whether a string contains a certain pattern.
 
 # Use the str_replace_all() function to replace all instances of one pattern with another pattern. To remove a pattern,
@@ -104,10 +103,12 @@ murders_raw %>% summarize_all(funs(commas))
 # replace commas with the empty string and convert to numeric
 test_1 <- str_replace_all(murders_raw$population, ",", "")
 test_1 <- as.numeric(test_1)
-
+test_1
 # parse_number also removes commas and converts to numeric
 test_2 <- parse_number(murders_raw$population)
-identical(test_1, test_2)
+test_2
+
+identical(test_1, test_2).
 
 murders_new <- murders_raw %>% mutate_at(2:3, parse_number)
 murders_new %>% head
