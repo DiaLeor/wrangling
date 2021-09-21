a7fc5143f69a8fa236d77dd9770449b737a9dfc0
Dia Leor Paule <dia.paule@gmail.com>
2021-09-21 19:57:53 +0800

    Update Section 4 notes on dates, times, and text mining

--- time_text_mining/time-txt.R
+++ time_text_mining/time-txt.R
@@ -166,6 +166,8 @@ campaign_tweets %>%
        y = "% of tweets",
        color = "")
 
+ggsave("time_text_mining/trump-tweets-graph.png")
+
 # We notice a big peak for the Android in early hours of the morning, between 6 and 8 AM. There seems to be a clear
 # different in these patterns. We will therefore assume that two different entities are using these two devices. Now we
 # will study how their tweets differ. To do this we introduce the tidytext package.
@@ -247,8 +249,8 @@ tweet_words %>%
              !str_detect(word, "^\\d+$")) %>%
     mutate(word = str_replace(word, "^'", ""))
 
-  # Now that we have all our words in a table, along with information about what device was used to compose the tweet they
-  # came from, we can start exploring which words are more common when comparing Android to iPhone.
+# Now that we have all our words in a table, along with information about what device was used to compose the tweet they
+# came from, we can start exploring which words are more common when comparing Android to iPhone.
   
 # For each word we want to know if it is more likely to come from an Android tweet or an iPhone tweet. We previously
 # introduced the odds ratio, a summary statistic useful for quantifying these differences. For each device and a given
@@ -292,6 +294,7 @@ get_sentiments("bing")
 # The AFINN lexicon assigns a score between -5 and 5, with -5 the most negative and 5 the most positive.
 get_sentiments("afinn")
 
+
 # The loughran and nrc lexicons provide several different sentiments:
 get_sentiments("loughran") %>% count(sentiment)
 get_sentiments("nrc") %>% count(sentiment)
@@ -299,14 +302,15 @@ get_sentiments("nrc") %>% count(sentiment)
 # To start learning about how these lexicons were developed, read this help file: ?sentiments.
 
 # For the analysis here we are interested in exploring the different sentiments of each tweet, so we will use the
-# nrc lexicon:
-nrc <- get_sentiments("nrc") %>%
-  select(word, sentiment)
+# loughran lexicon:
+loughran <- get_sentiments("loughran") %>%
+    select(word, sentiment)
+
 
 # We can combine the words and sentiments using inner_join(), which will only keep words associated with a sentiment.
 # Here are 10 random words extracted from the tweets:
-tweet_words %>% inner_join(nrc, by = "word") %>% 
-  select(source, word, sentiment) %>% sample_n(10)
+tweet_words %>% inner_join(loughran, by = "word") %>%
+    select(source, word, sentiment) %>% sample_n(10)
 
 # Now we are ready to perform a quantitative analysis comparing Android and iPhone by comparing the sentiments of the
 # tweets posted from each device. Here we could perform a tweet by tweet analysis, assigning a sentiment to each tweet.
@@ -314,12 +318,13 @@ tweet_words %>% inner_join(nrc, by = "word") %>%
 # in the lexicon. For illustrative purposes, we will perform a much simpler analysis: we will count and compare the
 # frequencies of each sentiment appears for each device.
 sentiment_counts <- tweet_words %>%
-  left_join(nrc, by = "word") %>%
+  left_join(loughran, by = "word") %>%
   count(source, sentiment) %>%
   spread(source, n) %>%
   mutate(sentiment = replace_na(sentiment, replace = "none"))
-sentiment_counts
 
+  sentiment_counts
+  
 # Because more words were used on the Android than on the phone:
 tweet_words %>% group_by(source) %>% summarize(n = n())
 
@@ -352,8 +357,10 @@ log_or %>%
   ggplot(aes(x = sentiment, ymin = conf.low, ymax = conf.high)) +
   geom_errorbar() +
   geom_point(aes(sentiment, log_or)) +
-  ylab("Log odds ratio for association between Android and sentiment") +
+  ylab("Log odds ratio for assoc between Android and sentiment") +
   coord_flip() 
+
+ggsave("time_text_mining/sentiment-plot.png")
 
 # We see that the disgust, anger, negative sadness and fear sentiments are associated with the Android in a way that is
 # hard to explain by chance alone. Words not associated to a sentiment were strongly associated with the iPhone source,
@@ -361,12 +368,12 @@ log_or %>%
 
 # If we are interested in exploring which specific words are driving these differences, we can back to our
 # android_iphone_or object:
-android_iphone_or %>% inner_join(nrc) %>%
+android_iphone_or %>% inner_join(loughran) %>%
   filter(sentiment == "disgust" & Android + iPhone > 10) %>%
   arrange(desc(or))
 
 # We can make a graph:
-android_iphone_or %>% inner_join(nrc, by = "word") %>%
+android_iphone_or %>% inner_join(loughran, by = "word") %>%
   mutate(sentiment = factor(sentiment, levels = log_or$sentiment)) %>%
   mutate(log_or = log(or)) %>%
   filter(Android + iPhone > 10 & abs(log_or)>1) %>%
@@ -376,4 +383,4 @@ android_iphone_or %>% inner_join(nrc, by = "word") %>%
   geom_bar(stat="identity", show.legend = FALSE) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) 
 
-ggsave("time_text_mining/trump-tweets-graph.png")
+ggsave("time_text_mining/sentiment-graph.png")
